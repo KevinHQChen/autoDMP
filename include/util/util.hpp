6eab8c79486f683eddd40b848a4bcd19e7ee89c0
khqc <khqc@khqc.ca>
2022-08-28 20:55:50 +0000

    switch to toml config file over cli parameters
    (also added a cool type checking function for debug purposes)

--- include/util/util.hpp
+++ include/util/util.hpp
@@ -19,12 +19,35 @@
 #include "opencv2/core/cuda.hpp"
 #include <Eigen/Dense>
 
-#include <toml++/toml.h>
 #include <CLI/CLI.hpp>
 #include <fmt/format.h>
 #include <fmt/ostream.h>
 #include <spdlog/spdlog.h>
+#include <toml.hpp>
 
+#include <string_view>
+
+// usage (print variable type in human-readable format): info("var type: {}",
+// type_name<decltype(var)>());
+template <typename T> constexpr auto type_name() {
+  std::string_view name, prefix, suffix;
+#ifdef __clang__
+  name = __PRETTY_FUNCTION__;
+  prefix = "auto type_name() [T = ";
+  suffix = "]";
+#elif defined(__GNUC__)
+  name = __PRETTY_FUNCTION__;
+  prefix = "constexpr auto type_name() [with T = ";
+  suffix = "]";
+#elif defined(_MSC_VER)
+  name = __FUNCSIG__;
+  prefix = "auto __cdecl type_name<";
+  suffix = ">(void)";
+#endif
+  name.remove_prefix(prefix.size());
+  name.remove_suffix(suffix.size());
+  return name;
+}
 
 struct config {
   CLI::App cli{fmt::format("{} version {}", "project name", "0.0.1")};
