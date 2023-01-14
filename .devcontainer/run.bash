55f6ef5f764fbf0087431c0ef8e865dcdd8b4010
khqc <khqc@khqc.ca>
2023-01-13 21:43:36 -0500

    don't add pump if it's not connected

--- .devcontainer/run.bash
+++ .devcontainer/run.bash
@@ -8,11 +8,21 @@ else
     exit 1
 fi
 
+container_name=$(echo $1 | sed -E 's/[^a-zA-Z0-9_.-]//g')
+
 export DISPLAY=:0.0
 xhost +local:docker
 
+# check if piezopump exists
+if test -e /dev/ttyACM0; then
+    export TTYACM0=/dev/ttyACM0
+else
+    export TTYACM0=/dev/null
+fi
+
 # --volume "$GITROOT:/app" \
 docker run \
+    --name $container_name \
     --network=host \
     --env="DISPLAY=$DISPLAY" \
     --env SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
@@ -22,8 +32,9 @@ docker run \
     --volume "/dev/video0:/dev/video0" \
     --volume "/dev/video1:/dev/video1" \
     --volume "/dev/bus/usb:/dev/bus/usb" \
-    --volume "/dev/ttyACM0:/dev/ttyACM0" \
+    --volume $TTYACM0:$TTYACM0 \
     -p 8080:80 \
     -it \
     --privileged \
+    --rm \
     $1
