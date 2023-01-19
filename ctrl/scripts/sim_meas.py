ee9d5d7514f2403cef7838a7ecf99c7923f6f116
khqc <khqc@khqc.ca>
2023-01-19 19:58:57 +0000

    control library testing

--- ctrl/scripts/sim_meas.py
+++ ctrl/scripts/sim_meas.py
@@ -1,7 +1,68 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import control
-from control import tf, ss, mixsyn, step_response
+from numpy.linalg import matrix_rank as rank
+import control as ct
+
+# Define a 2-input, 2-output unstable second order underdamped system
+dt = 0.025
+zeta = 0.5
+wn = 10
+num = [wn**2]
+num1 = [wn**2, 2*zeta*wn]
+den = [1, 2*zeta*wn, wn**2, 0]
+sysc_tf = ct.tf([[num, num1],
+             [num1, num]],
+            [[den, den],
+             [den, den]])
+sysc = ct.minreal(ct.ss(sysc_tf))
+sysd = ct.sample_system(sysc, dt, method='zoh')
+
+t, y = ct.step_response(sysd, T=np.arange(0, 10, dt), squeeze=True)
+
+# Plot the step response on 4 subplots with legends
+plt.figure()
+plt.subplot(2, 2, 1)
+plt.plot(t, y[0, 0, :], label='input 1 to output 1')
+plt.legend()
+plt.subplot(2, 2, 2)
+plt.plot(t, y[0, 1, :], label='input 1 to output 2')
+plt.legend()
+plt.subplot(2, 2, 3)
+plt.plot(t, y[1, 0, :], label='input 2 to output 1')
+plt.legend()
+plt.subplot(2, 2, 4)
+plt.plot(t, y[1, 1, :], label='input 2 to output 2')
+plt.legend()
+plt.show()
+
+t = np.arange(0, 100, dt)
+# Square input
+u = np.sign(np.sin(2*np.pi*t))
+u = np.vstack((u, u)).T
+
+# Initial conditions
+x = np.zeros((len(t), rank(sysd.A)))
+y = np.zeros((len(t), rank(sysd.C)))
+
+# Simulate the system
+for i in range(1, len(t)):
+    x[i, :] = np.dot(sysd.A, x[i-1, :]) + np.dot(sysd.B, u[i-1, :])
+    y[i, :] = np.dot(sysd.C, x[i, :]) + np.dot(sysd.D, u[i-1, :])
+
+plt.figure()
+plt.subplot(1, 2, 1)
+plt.plot(t, u[:, 0], label='input 1')
+plt.plot(t, y[:, 0], label='output 1')
+plt.legend()
+plt.subplot(1, 2, 2)
+plt.plot(t, u[:, 1], label='input 2')
+plt.plot(t, y[:, 1], label='output 2')
+plt.legend()
+plt.show()
+
+
+
+
 
 # Define the plant
 dt = 0.025
@@ -35,11 +96,6 @@ plt.subplot(2, 2, 4)
 plt.plot(t, y[1, 1, :], label='input 2 to output 2')
 plt.legend()
 plt.show()
-
-
-
-
-
 
 # Define the controller
 
