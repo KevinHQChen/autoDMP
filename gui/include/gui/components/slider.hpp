f1639d05dbace157324c9b116ec3c8ce2fe7f2b2
khqc <khqc@khqc.ca>
2023-05-06 00:46:28 +0000

    slider library refactoring

--- gui/include/gui/components/slider.hpp
+++ gui/include/gui/components/slider.hpp
@@ -12,51 +12,48 @@ namespace gui {
 template <typename T> class Slider {
   std::string label_;
   T min_, max_;
-  T *value_{nullptr};
+  T *value_;
+  bool internalValue_{false};
   std::string format_;
   ImVec2 sliderSize_;
   bool horizontal_;
   std::function<void()> callback_;
 
 public:
-  Slider(std::string label, T min, T max, T *value, std::string format = "%d",
-         ImVec2 sliderSize = ImVec2(36, 200), bool horizontal = true,
-         std::function<void()> callback = nullptr)
+  Slider(
+      std::string label, T min, T max, T *value = nullptr, std::string format = "%d",
+      ImVec2 sliderSize = ImVec2(36, 200), bool horizontal = true,
+      std::function<void()> callback = []() {})
       : label_(label), min_(min), max_(max), format_(format), value_(value),
         sliderSize_(sliderSize), horizontal_(horizontal), callback_(callback) {
-    if (value_ == nullptr)
+    if (value_ == nullptr) {
       value_ = new T(min_);
-    assert(value_ != nullptr);
+      internalValue_ = true;
+    }
   }
 
-  template <typename U = T>
-  typename std::enable_if<std::is_integral<U>::value, void> renderIntSlider(std::string label) {
-    if (horizontal_)
-      ImGui::SliderInt(label.c_str(), (int *)value_, min_, max_, format_.c_str());
-    else
-      ImGui::VSliderInt(label.c_str(), sliderSize_, (int *)value_, min_, max_, format_.c_str());
-  }
-
-  template <typename U = T>
-  typename std::enable_if<std::is_floating_point<U>::value, void>
-  renderFloatSlider(std::string label) {
-    if (horizontal_)
-      ImGui::SliderFloat(label.c_str(), (float *)value_, min_, max_);
-    else
-      ImGui::VSliderFloat(label.c_str(), sliderSize_, (float *)value_, min_, max_);
+  ~Slider() {
+    if (internalValue_)
+      delete value_;
   }
 
   void render() {
     ImGui::BeginGroup();
     // TODO set slider size:
     // ImGui::SetNextItemWidth(ImGui::GetFontSize() * 8);
-    if (std::is_integral<T>::value)
-      renderIntSlider<T>(label_);
-    else if (std::is_floating_point<T>::value)
-      renderFloatSlider<T>(label_);
+    if (horizontal_) {
+      if constexpr (std::is_same<T, int>::value)
+        ImGui::SliderInt(label_.c_str(), value_, min_, max_, format_.c_str());
+      else if constexpr (std::is_same<T, float>::value)
+        ImGui::SliderFloat(label_.c_str(), value_, min_, max_);
+    } else {
+      if constexpr (std::is_same<T, int>::value)
+        ImGui::VSliderInt(label_.c_str(), sliderSize_, value_, min_, max_, format_.c_str());
+      else if constexpr (std::is_same<T, float>::value)
+        ImGui::VSliderFloat(label_.c_str(), sliderSize_, value_, min_, max_);
+    }
     ImGui::EndGroup();
-    if (callback_)
-      callback_();
+    callback_();
   }
 
   T get() const { return *value_; }
@@ -68,43 +65,32 @@ template <typename T> class SliderArray {
   std::string label_;
   T min_, max_;
   int numSliders_;
-  std::vector<T> *values_{nullptr};
+  std::vector<T> *values_;
+  bool internalValues_{false};
   std::string format_;
   ImVec2 sliderSize_;
   bool horizontal_;
   std::function<void()> callback_;
 
 public:
-  SliderArray(std::string label, T min, T max, std::vector<T> *values = nullptr, int numSliders = 1,
-              std::string format = "%d", ImVec2 sliderSize = ImVec2(36, 200),
-              bool horizontal = true, std::function<void()> callback = nullptr)
+  SliderArray(
+      std::string label, T min, T max, std::vector<T> *values = nullptr, int numSliders = 1,
+      std::string format = "%d", ImVec2 sliderSize = ImVec2(36, 200), bool horizontal = true,
+      std::function<void()> callback = []() {})
       : label_(label), min_(min), max_(max), values_(values), format_(format),
         sliderSize_(sliderSize), horizontal_(horizontal), callback_(callback) {
     if (values_ == nullptr) {
       values_ = new std::vector<T>(numSliders, min_);
+      internalValues_ = true;
       numSliders_ = numSliders;
     } else
       numSliders_ = values_->size();
     assert(values_->size() == numSliders_);
   }
 
-  template <typename U = T>
-  typename std::enable_if<std::is_integral<U>::value, void> renderIntSlider(std::string label,
-                                                                            int idx) {
-    if (horizontal_)
-      ImGui::SliderInt(label.c_str(), (int *)&(*values_)[idx], min_, max_, format_.c_str());
-    else
-      ImGui::VSliderInt(label.c_str(), sliderSize_, (int *)&(*values_)[idx], min_, max_,
-                        format_.c_str());
-  }
-
-  template <typename U = T>
-  typename std::enable_if<std::is_floating_point<U>::value, void>
-  renderFloatSlider(std::string label, int idx) {
-    if (horizontal_)
-      ImGui::SliderFloat(label.c_str(), (float *)&(*values_)[idx], min_, max_);
-    else
-      ImGui::VSliderFloat(label.c_str(), sliderSize_, (float *)&(*values_)[idx], min_, max_);
+  ~SliderArray() {
+    if (internalValues_)
+      delete values_;
   }
 
   void render() {
@@ -112,16 +98,22 @@ public:
     ImGui::Text("%s", label_.c_str());
     for (int i = 0; i < numSliders_; ++i) {
       std::string label = "##" + label_ + std::to_string(i);
-      if (std::is_integral<T>::value)
-        renderIntSlider<T>(label, i);
-      else if (std::is_floating_point<T>::value)
-        renderFloatSlider<T>(label, i);
-      if (!horizontal_)
+      if (horizontal_) {
+        if constexpr (std::is_same<T, int>::value)
+          ImGui::SliderInt(label.c_str(), &(*values_)[i], min_, max_, format_.c_str());
+        else if constexpr (std::is_same<T, float>::value)
+          ImGui::SliderFloat(label.c_str(), &(*values_)[i], min_, max_);
+      } else {
+        if constexpr (std::is_same<T, int>::value)
+          ImGui::VSliderInt(label.c_str(), sliderSize_, &(*values_)[i], min_, max_,
+                            format_.c_str());
+        else if constexpr (std::is_same<T, float>::value)
+          ImGui::VSliderFloat(label.c_str(), sliderSize_, &(*values_)[i], min_, max_);
         ImGui::SameLine();
+      }
     }
     ImGui::EndGroup();
-    if (callback_)
-      callback_();
+    callback_();
   }
 
   T get(int index) const { return (*values_)[index]; }
