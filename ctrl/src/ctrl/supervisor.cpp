047b2e413dd8d402613d9d7e0fd05d214773098f
khqc <khqc@khqc.ca>
2023-05-08 02:13:16 +0000

    state transitions work in simulation mode

--- ctrl/src/ctrl/supervisor.cpp
+++ ctrl/src/ctrl/supervisor.cpp
@@ -82,27 +82,25 @@ void Supervisor::stopThread() {
 }
 
 bool Supervisor::measAvail() {
-  // if no channels are being observed, observe ch0
-  if (std::accumulate(supOut.currEv.chs, supOut.currEv.chs + Config::numChans_, 0) == 0)
-    supOut.currEv.chs[0] = true;
-
   allMeasAvail = true;
   anyMeasAvail = false;
   simMeasAvail = duration_cast<milliseconds>(steady_clock::now() - prevCtrlTime).count() >= 25;
   for (int ch = 0; ch < Config::numChans_; ++ch) {
-    trueMeasAvail[ch] = false;
+    if (!simModeActive)
+      trueMeasAvail[ch] = !imProc->procDataQArr[ch]->empty();
+    else {
+      if (supOut.currEv.chs[ch] == supOut.currEv.nextChs[ch])
+        trueMeasAvail[ch] = simMeasAvail && supOut.currEv.chs[ch];
+      else
+        trueMeasAvail[ch] = !supOut.inTransRegion ? (simMeasAvail && supOut.currEv.chs[ch])
+                                                  : (simMeasAvail && supOut.currEv.nextChs[ch]);
+    }
+
     if (supOut.currEv.chs[ch]) {
-      trueMeasAvail[ch] =
-          !simModeActive
-              ? !imProc->procDataQArr[ch]->empty()
-              : duration_cast<milliseconds>(steady_clock::now() - prevCtrlTime).count() >= 25;
-      allMeasAvail &= trueMeasAvail[ch];
       anyMeasAvail |= trueMeasAvail[ch];
+      allMeasAvail &= trueMeasAvail[ch];
     }
   }
-
-  // info("allMeasAvail: {}, anyMeasAvail: {}, simMeasAvail: {}", allMeasAvail, anyMeasAvail,
-  //      simMeasAvail);
 
   if (allMeasAvail) {
     supIn.inputevents[0] = !supIn.inputevents[0];
@@ -116,15 +114,30 @@ bool Supervisor::measAvail() {
 
 bool Supervisor::updateInputs() {
   for (int ch = 0; ch < Config::numChans_; ++ch) {
-    if (supOut.currEv.chs[ch]) {
-      if (trueMeasAvail[ch]) {
-        supIn.y[ch] = !simModeActive ? imProc->procDataQArr[ch]->get().loc.y : supOut.yhat[ch];
-      } else // simMeasAvail (set y to 0 to tell the observer to only predict for this channel)
-        supIn.y[ch] = 0;
-      info("y[{}]: {}", ch, supIn.y[ch]);
-    } else // we're not observing this channel
+    if (trueMeasAvail[ch]) {
+      if (!simModeActive)
+        supIn.y[ch] = imProc->procDataQArr[ch]->get().loc.y;
+      else {
+        if (supOut.currEv.chs[ch] == supOut.currEv.nextChs[ch])
+          supIn.y[ch] = supOut.yhat[ch];
+        else
+          supIn.y[ch] = !supOut.inTransRegion ? supOut.yhat[ch] : supIn.y_o[ch];
+      }
+
+    } else // simMeasAvail (set y to 0 to tell the observer to only predict for this channel)
       supIn.y[ch] = 0;
   }
+
+  ctrlDataQueuePtr->out << "allMA: " << allMeasAvail << ", anyMA: " << anyMeasAvail
+                        << ", simMA: " << simMeasAvail << ", inTR: " << (bool)supOut.inTransRegion
+                        << ", y: " << (double)supIn.y[0] << ", " << (double)supIn.y[1] << ", "
+                        << (double)supIn.y[2] << ", yhat: " << (double)supOut.yhat[0] << ", "
+                        << (double)supOut.yhat[1] << ", " << (double)supOut.yhat[2]
+                        << ", u: " << (double)supOut.u[0] << ", " << (double)supOut.u[1] << ", "
+                        << (double)supOut.u[2] << ", chs: " << (bool)supOut.currEv.chs[0]
+                        << (bool)supOut.currEv.chs[1] << (bool)supOut.currEv.chs[2]
+                        << ", nextChs: " << (bool)supOut.currEv.nextChs[0]
+                        << (bool)supOut.currEv.nextChs[1] << (bool)supOut.currEv.nextChs[2] << "\n";
 
   if (supOut.requestEvent && !evQueue_->empty())
     supIn.nextEv = evQueue_->get();
@@ -138,45 +151,45 @@ void Supervisor::start() {
   while (startedCtrl) {
     if (measAvail()) {
       prevCtrlTime = steady_clock::now();
-      info("Time: {}", duration_cast<milliseconds>(prevCtrlTime - initTime).count());
+      // info("Time: {}", duration_cast<milliseconds>(prevCtrlTime - initTime).count());
       updateInputs();
       sup->rtU = supIn;
-      info("y0: {}", sup->rtU.y[0]);
-      info("y1: {}", sup->rtU.y[1]);
-      info("y2: {}", sup->rtU.y[2]);
-      info("y_max0: {}", sup->rtU.y_max[0]);
-      info("y_max1: {}", sup->rtU.y_max[1]);
-      info("y_max2: {}", sup->rtU.y_max[2]);
-      info("y_o0: {}", sup->rtU.y_o[0]);
-      info("y_o1: {}", sup->rtU.y_o[1]);
-      info("y_o2: {}", sup->rtU.y_o[2]);
-      info("u_o0: {}", sup->rtU.u_o[0]);
-      info("u_o1: {}", sup->rtU.u_o[1]);
-      info("u_o2: {}", sup->rtU.u_o[2]);
-      info("y_range0: {}", sup->rtU.y_range[0]);
-      info("y_range1: {}", sup->rtU.y_range[1]);
-      info("enAdapt: {}", sup->rtU.enAdapt);
-      info("u0: {}", sup->rtY.u[0]);
-      info("u1: {}", sup->rtY.u[1]);
-      info("u2: {}", sup->rtY.u[2]);
-      info("yhat0: {}", sup->rtY.yhat[0]);
-      info("yhat1: {}", sup->rtY.yhat[1]);
-      info("yhat2: {}", sup->rtY.yhat[2]);
-      info("inTransRegion: {}", sup->rtY.inTransRegion);
-      info("requestEvent: {}", sup->rtY.requestEvent);
-      info("currEv srcState: {}", sup->rtY.currEv.srcState);
-      info("currEv destState: {}", sup->rtY.currEv.destState);
-      info("currEv destPos0: {}", sup->rtY.currEv.destPos[0]);
-      info("currEv destPos1: {}", sup->rtY.currEv.destPos[1]);
-      info("currEv destPos2: {}", sup->rtY.currEv.destPos[2]);
-      info("currEv moveTime: {}", sup->rtY.currEv.moveTime);
-      info("currEv holdTime: {}", sup->rtY.currEv.holdTime);
-      info("currEv chs0: {}", sup->rtY.currEv.chs[0]);
-      info("currEv chs1: {}", sup->rtY.currEv.chs[1]);
-      info("currEv chs2: {}", sup->rtY.currEv.chs[2]);
-      info("currEv nextChs0: {}", sup->rtY.currEv.nextChs[0]);
-      info("currEv nextChs1: {}", sup->rtY.currEv.nextChs[1]);
-      info("currEv nextChs2: {}", sup->rtY.currEv.nextChs[2]);
+      // info("y0: {}", sup->rtU.y[0]);
+      // info("y1: {}", sup->rtU.y[1]);
+      // info("y2: {}", sup->rtU.y[2]);
+      // info("y_max0: {}", sup->rtU.y_max[0]);
+      // info("y_max1: {}", sup->rtU.y_max[1]);
+      // info("y_max2: {}", sup->rtU.y_max[2]);
+      // info("y_o0: {}", sup->rtU.y_o[0]);
+      // info("y_o1: {}", sup->rtU.y_o[1]);
+      // info("y_o2: {}", sup->rtU.y_o[2]);
+      // info("u_o0: {}", sup->rtU.u_o[0]);
+      // info("u_o1: {}", sup->rtU.u_o[1]);
+      // info("u_o2: {}", sup->rtU.u_o[2]);
+      // info("y_range0: {}", sup->rtU.y_range[0]);
+      // info("y_range1: {}", sup->rtU.y_range[1]);
+      // info("enAdapt: {}", sup->rtU.enAdapt);
+      // info("u0: {}", sup->rtY.u[0]);
+      // info("u1: {}", sup->rtY.u[1]);
+      // info("u2: {}", sup->rtY.u[2]);
+      // info("yhat0: {}", sup->rtY.yhat[0]);
+      // info("yhat1: {}", sup->rtY.yhat[1]);
+      // info("yhat2: {}", sup->rtY.yhat[2]);
+      // info("inTransRegion: {}", sup->rtY.inTransRegion);
+      // info("requestEvent: {}", sup->rtY.requestEvent);
+      // info("currEv srcState: {}", sup->rtY.currEv.srcState);
+      // info("currEv destState: {}", sup->rtY.currEv.destState);
+      // info("currEv destPos0: {}", sup->rtY.currEv.destPos[0]);
+      // info("currEv destPos1: {}", sup->rtY.currEv.destPos[1]);
+      // info("currEv destPos2: {}", sup->rtY.currEv.destPos[2]);
+      // info("currEv moveTime: {}", sup->rtY.currEv.moveTime);
+      // info("currEv holdTime: {}", sup->rtY.currEv.holdTime);
+      // info("currEv chs0: {}", sup->rtY.currEv.chs[0]);
+      // info("currEv chs1: {}", sup->rtY.currEv.chs[1]);
+      // info("currEv chs2: {}", sup->rtY.currEv.chs[2]);
+      // info("currEv nextChs0: {}", sup->rtY.currEv.nextChs[0]);
+      // info("currEv nextChs1: {}", sup->rtY.currEv.nextChs[1]);
+      // info("currEv nextChs2: {}", sup->rtY.currEv.nextChs[2]);
 
       sup->step();
       supOut = sup->rtY;
