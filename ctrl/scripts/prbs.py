aca197c2d854af119cae72aada7b24b3c27e0c20
khqc <khqc@khqc.ca>
2023-08-03 19:37:37 +0000

    multi-channel PRBS makes more sense
    (if you want single channel just use one tap)

--- ctrl/scripts/prbs.py
+++ ctrl/scripts/prbs.py
@@ -14,10 +14,10 @@ def prbs(minVal, maxVal, order):
     function that generates a PRBS sequence for a single channel.
     Parameters
     ----------
-    minVal : float
-        Minimum value of PRBS sequence.
-    maxVal : float
-        Maximum value of PRBS sequence.
+    minVal : float_array_like
+        Minimum value of PRBS sequence for each channel.
+    maxVal : float_array_like
+        Maximum value of PRBS sequence for each channel.
     order : int
         Order of PRBS sequence.
     Returns
@@ -25,48 +25,22 @@ def prbs(minVal, maxVal, order):
     prbs : array_like
         PRBS sequence.
     """
+
+    # select taps for each pump to ensure each maximum length sequence is uncorrelated
+    # (max_len_seq skips first tap; it is always equal to the order)
+    taps = [
+        (7),           # 2 taps
+        (9, 8, 5),     # 4 taps
+        (9, 8, 7, 5, 4) # 6 taps
+    ]
     print('Generating PRBS sequence...')
-    prbs_1d = max_len_seq(order)[0] * (maxVal - minVal) + minVal
-    prbs = np.array(prbs_1d)
+
+    prbs = []
+    for i, tap in enumerate(taps):
+        prbs_1d = max_len_seq(order, taps=tap)[0] * (maxVal[i] - minVal[i]) + minVal[i]
+        prbs.append(prbs_1d)
+
+    # convert to numpy array
+    prbs = np.array(prbs)
 
     return prbs
-
-# def prbs(selCh, minVal, maxVal, order, flip=True):
-#     """
-#     function that generates a PRBS sequence.
-#     Parameters
-#     ----------
-#     selCh : bool array
-#         Channels to generate PRBS sequence for.
-#     minVal : array_like
-#         Minimum value of PRBS sequence for each channel.
-#     maxVal : array_like
-#         Maximum value of PRBS sequence for each channel.
-#     order : int
-#         Order of PRBS sequence.
-#     Returns
-#     -------
-#     prbs : array_like
-#         PRBS sequence.
-#     """
-#     print(selCh)
-#     numCh = selCh.count(True)
-
-#     # select taps for each pump to ensure each maximum length sequence is uncorrelated
-#     # (max_len_seq skips first tap; it is always equal to the order)
-#     taps = [
-#         (7),           # 2 taps
-#         (9, 8, 5),     # 4 taps
-#         (9, 8, 7, 5, 4) # 6 taps
-#     ]
-
-#     print('Generating PRBS sequence...')
-#     prbs = []
-#     for i, tap in enumerate(taps):
-#         prbs_1d = max_len_seq(order, taps=tap)[0] * (maxVal[i] - minVal[i]) + minVal[i]
-#         prbs.append(prbs_1d)
-
-#     # convert to numpy array
-#     prbs = np.array(prbs)
-
-#     return prbs
