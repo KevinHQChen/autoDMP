24f1113c67a581ebc239a7e5a614f34eb4cf0025
d2hahn <dylan.hahn@uwaterloo.ca>
2023-10-20 21:36:37 +0000

    added Pressure_Pump_Input_Single_Value struct and Pressure_Pump_Input class to header file in autoDMP/pump_pressure_inputs/pump_pressure_inputs.hpp also added constructor definition for Pressure_Pump_Input class, these features need to be tested, did not create list file for compilation yet.

--- pump_pressure_inputs/include/pump_pressure_inputs.hpp
+++ pump_pressure_inputs/include/pump_pressure_inputs.hpp
@@ -1,15 +1,19 @@
 #pragma once
 #include "util/util.hpp"
 
-class Pressure_Pump_Input{
+struct Pressure_Pump_Input_Single_Value{
 
     public:
     //public member variables
         double Time;
         double Pressure;
-    //public member functions
-        std::vector<Pressure_Pump_Input> text_to_vector(std::string filepath);
-    
+}
 
+class Pressure_Pump_Input{
 
-};
+    public:
+    //public member variables
+        std::vector<Pressure_Pump_Input_Single_Value> input;
+    //public constructor
+        Pressure_Pump_Input (std::string filepath);
+}
---
+++ pump_pressure_inputs/src/pump_pressure_inputs.cpp
@@ -0,0 +1,17 @@
+#include "pump_pressure_inputs/pump_pressure_inputs.hpp"
+
+Pressure_Pump_Input::Pressure_Pump_Input(std::string filepath)
+{
+    std::ifstream file;
+    file.open(filepath);
+    
+    double t, p;
+
+    while(file >> t >> p)
+    {
+        input.push_back(Pressure_Pump_Input_Single_Value{t, p});
+    }
+    file.close();
+
+}
+    
