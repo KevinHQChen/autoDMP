b87539fc9ca8b4ba51f26618c88514effc440a0d
d2hahn <dylan.hahn@uwaterloo.ca>
2023-10-20 15:14:04 +0000

    added listfile to pump_sinewave directory that initiates build of the conv_txt_fn_test executable, this subdirectory was then added to the top level list file to allow the build to take place. Creates executable whose path is  /autoDMP/build/pump_sinewave/Release/sinvec.

--- pump_sinewave/sinewave_txt_to_vector_fn/conv_txt_to_vec_fn.hpp
+++ pump_sinewave/sinewave_txt_to_vector_fn/conv_txt_to_vec_fn.hpp
@@ -1,10 +1,4 @@
-#ifndef CONV_TXT_TO_VEC_FN_HPP
-#define CONV_TXT_TO_VEC_FN_HPP
-
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
+#pragma once
+#include "util/util.hpp"
 
 std::vector<double> conv_txt_to_vec_fn(std::string filepath);
-#endif
