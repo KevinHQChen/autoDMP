0b855f14f31f47756227eee5c84556a99c56ee7e
d2hahn <dylan.hahn@uwaterloo.ca>
2023-10-20 22:02:48 +0000

    finished class structure for Pressure_Pump_Input, also edited CMake file, something is wrong with it, having trouble finding the directories for the pump_pressure_inputs.hpp header file, will need to look into it later.

---
+++ pump_pressure_inputs/test/class_test.cpp
@@ -0,0 +1,11 @@
+#include "pump_pressure_inputs/pump_pressure_inputs.hpp"
+
+int main(){
+    Pressure_Pump_Input test_input('/autoDMP/pump_pressure_inputs/sinewave_txt_file_generation/p_vs_t.txt');
+    
+    for (auto file_line : test_input)
+        std::cout << file_line.Time << ','<< file_line.Pressure << std::endl;
+
+    return 0;
+}
+
